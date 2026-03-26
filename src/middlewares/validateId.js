d3bb352a260307e39d475829dde403c12cc0eb3e
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-03-26 19:44:44 +0600

    modified

--- src/middlewares/validateId.js
+++ src/middlewares/validateId.js
@@ -3,10 +3,6 @@ import mongoose from "mongoose";
 export const validateId = (req, res, next) => {
   const id = req.params.id?.trim();
 
-  if (!id) {
-    return res.status(400).send({ success: false, message: "ID is required" });
-  }
-
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({ success: false, message: "Invalid ID" });
   }
